d1e1d9de2adb702ea029377b5f7b3a2904ebcfba
Elena Kleinwort <you@example.com>
2020-09-29 23:29:46 +0200

    New version of drei_dimensionen with Punkt class

---
+++ Punkt.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+
+// Wir definiern eine neue Klasse (Datentype) namens Punkt
+class Punkt {
+// Alle Dinge, auf die man von aussen zugreifen darf:
+public:
+    /**
+     * "Konstruktor" - Funktion, die aufgerufen wird, wenn wir ein neues Objekt vom Typ "Punkt" erzeugen
+     * @param x X-Koorinate
+     * @param y Y-Koorinate
+     */
+    Punkt(double x, double y, double z) {
+        // Wir speichern die Parameter des Konstruktors in den "privaten" Variablen x_ y_ und z_:
+        x_ = x;
+        y_ = y;
+        z_ = z;
+    };
+
+    Punkt() {
+      x_ = 0;
+      y_ = 0;
+      z_ = 0;
+    }
+
+    // Wir rufen die "private" Variable x_ ab:
+    double x() const { return x_; }
+    // Wir rufen die "private" Variable y_ ab:
+    double y() const { return y_; }
+    double z() const { return z_; }
+
+    friend std::ostream& operator<<(std::ostream& os, const Punkt& p);
+// Dinge, auf die nur der Punkt selbst Zuriff hat:
+private:
+    double x_;
+    double y_;
+    double z_;
+};
+
+
+Punkt operator-(const Punkt &a, const Punkt &b) {
+  return Punkt(a.x() - b.x(),a.y() - b.y(),a.z() - b.z());
+}
+
+Punkt operator+(const Punkt &a, const Punkt &b) {
+  return Punkt(a.x() + b.x(),a.y() + b.y(),a.z() + b.z());
+}
+
+Punkt operator*(const double &a, const Punkt &b) {
+  return Punkt(a * b.x(),a * b.y(),a * b.z());
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Punkt& p) {
+    os << p.x() << "," << p.y() << "," << p.z();
+    return os;
+}
